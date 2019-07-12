49adf61be0f8d66f13a7ffd305795541b33b1e20
carlos <carlosgilsaenz@gmail.com>
2019-07-12 11:16:17 -0700

    2

--- OneDrive_Run_As_Admin.bat
+++ OneDrive_Run_As_Admin.bat
@@ -24,6 +24,10 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\OneDrive" /v FilesOnDema
 :: disabe Personal OneDrive
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\OneDrive" /v DisablePersonalSync /t REG_DWORD /d "1" /f
 
+::tested and works!!!
+::Silently sign in users to the OneDrive
+reg add "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v SilentAccountConfig /t REG_DWORD /d "1" /f
+
 ::untested
 ::change default location
 ::[HKCU\SOFTWARE\Policies\Microsoft\OneDrive\DefaultRootDir] "1111-2222-3333-4444"="{User path}" 
@@ -32,8 +36,10 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\OneDrive" /v DisablePers
 ::untested
 ::teamsite library 
 ::[HKCU\Software\Policies\Microsoft\OneDrive\TenantAutoMount]"LibraryName"="LibraryID"
-reg add "HKCU\Software\Policies\Microsoft\OneDrive\TenantAutoMount" /v LibraryName /t REG_SZ /d "tenantId=67fa1239%2D84f7%2D4124%2Db0aa%2D72e3b5dda92c&siteId=%7B435eb7cb%2Db673%2D4914%2D9eee%2D979fc5d01515%7D&webId=%7B8636514f%2Def6d%2D47fc%2D8155%2Dba11a31cbda6%7D&listId=%7BE9319D64%2D26DA%2D4173%2DB250%2DE65908C87875%7D&folderId=fa73a209%2Da249%2D4bbc%2D946b%2D6154eaa69dac&webUrl=https%3A%2F%2Fpostholdings%2Esharepoint%2Ecom%2Fsites%2FPNC%2DIT%2DTEAM&version=1" /f
-reg add "HKEY_USERS\S-1-5-21-341536205-593477472-1287307213-8608\Software\Policies\Microsoft\OneDrive\TenantAutoMount" /v LibraryName /t REG_SZ /d "tenantId=67fa1239%2D84f7%2D4124%2Db0aa%2D72e3b5dda92c&siteId=%7B435eb7cb%2Db673%2D4914%2D9eee%2D979fc5d01515%7D&webId=%7B8636514f%2Def6d%2D47fc%2D8155%2Dba11a31cbda6%7D&listId=%7BE9319D64%2D26DA%2D4173%2DB250%2DE65908C87875%7D&folderId=fa73a209%2Da249%2D4bbc%2D946b%2D6154eaa69dac&webUrl=https%3A%2F%2Fpostholdings%2Esharepoint%2Ecom%2Fsites%2FPNC%2DIT%2DTEAM&version=1" /f
+::reg add "HKCU\Software\Policies\Microsoft\OneDrive\TenantAutoMount" /v LibraryName /t REG_SZ /d "tenantId=tenantId=67fa1239%2D84f7%2D4124%2Db0aa%2D72e3b5dda92c&siteId=%7B435eb7cb%2Db673%2D4914%2D9eee%2D979fc5d01515%7D&webId=%7B8636514f%2Def6d%2D47fc%2D8155%2Dba11a31cbda6%7D&listId=%7BE9319D64%2D26DA%2D4173%2DB250%2DE65908C87875%7D&folderId=66e89d66%2D4561%2D4f6b%2Db364%2D719455452ccc&webUrl=https%3A%2F%2Fpostholdings%2Esharepoint%2Ecom%2Fsites%2FPNC%2DIT%2DTEAM&version=1" /f
+::reg add "HKCU\Software\Policies\Microsoft\OneDrive\TenantAutoMount" /v LibraryName /t REG_DWORD /d "tenantId=tenantId=67fa1239%2D84f7%2D4124%2Db0aa%2D72e3b5dda92c&siteId=%7B435eb7cb%2Db673%2D4914%2D9eee%2D979fc5d01515%7D&webId=%7B8636514f%2Def6d%2D47fc%2D8155%2Dba11a31cbda6%7D&listId=%7BE9319D64%2D26DA%2D4173%2DB250%2DE65908C87875%7D&folderId=66e89d66%2D4561%2D4f6b%2Db364%2D719455452ccc&webUrl=https%3A%2F%2Fpostholdings%2Esharepoint%2Ecom%2Fsites%2FPNC%2DIT%2DTEAM&version=1" /f
+::reg add "HKEY_USERS\S-1-5-21-341536205-593477472-1287307213-8608\Software\Policies\Microsoft\OneDrive\TenantAutoMount" /v LibraryName /t REG_SZ /d "tenantId=67fa1239%2D84f7%2D4124%2Db0aa%2D72e3b5dda92c&siteId=%7B435eb7cb%2Db673%2D4914%2D9eee%2D979fc5d01515%7D&webId=%7B8636514f%2Def6d%2D47fc%2D8155%2Dba11a31cbda6%7D&listId=%7BE9319D64%2D26DA%2D4173%2DB250%2DE65908C87875%7D&folderId=fa73a209%2Da249%2D4bbc%2D946b%2D6154eaa69dac&webUrl=https%3A%2F%2Fpostholdings%2Esharepoint%2Ecom%2Fsites%2FPNC%2DIT%2DTEAM&version=1" /f
+reg add "HKLM\Software\Policies\Microsoft\OneDrive\TenantAutoMount" /v "AN-IT-TEAM" /t REG_SZ /d "tenantId=67fa1239%2D84f7%2D4124%2Db0aa%2D72e3b5dda92c&siteId=%7B435eb7cb%2Db673%2D4914%2D9eee%2D979fc5d01515%7D&webId=%7B8636514f%2Def6d%2D47fc%2D8155%2Dba11a31cbda6%7D&listId=%7BE9319D64%2D26DA%2D4173%2DB250%2DE65908C87875%7D&webUrl=https%3A%2F%2Fpostholdings%2Esharepoint%2Ecom%2Fsites%2FPNC%2DIT%2DTEAM&version=1" /f
 
 
 pause
